66e2db0bf900a98ebcc4c105f2dd1b5aab4d086c
techatdriwe <tech@driwe.in>
2025-08-14 18:21:24 +0530

    config update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "images.pexels.com",
+      "d2s8i866417m9.cloudfront.net",
+      "images10.gaadi.com",
+      "truckcdn.cardekho.com"
+    ]
+  }
 };
 
 export default nextConfig;
