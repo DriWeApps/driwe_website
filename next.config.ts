1cb9601387d3cf8d94f08c0e24c1e5b43a63aa9d
DriWeApps <cto@driwe.in>
2025-08-18 11:09:27 +0530

    Corrected next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,29 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "images.pexels.com",
-      "d2s8i866417m9.cloudfront.net",
-      "images10.gaadi.com",
-      "truckcdn.cardekho.com"
-    ]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "d2s8i866417m9.cloudfront.net",
+      },
+      {
+        protocol: "https",
+        hostname: "images10.gaadi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "truckcdn.cardekho.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
